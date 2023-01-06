b940b22921e6872ed402d6b95e2fe19702c02d17
Shubham3023 <shubhamverma3023@gmail.com>
2023-01-06 14:22:08 +0530

    created text transformer in utils

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ import joblib
 import os, sys
 from exception import CustomException
 from logger import logging
-import string
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+from utils import text_transformer
+
 
 ### loading model and scalar object
 logging.info("Reading model object from model.sav file")
@@ -14,33 +12,7 @@ model=joblib.load('Models\model.sav')
 logging.info("Reading Vectorizer object from vectorizer.sav file")
 vectorizer=joblib.load(r'Models\vectorizer.sav')
 
-stemmer= PorterStemmer()
 
-def text_transformer(in_text):
-    ### lowering the text
-    in_text=in_text.lower()
-    ### seperating each word
-    in_text=nltk.word_tokenize(in_text)
-    ### removing special characters from in_text
-    temp_list=[]
-    for word in in_text:
-        if word.isalnum():
-            temp_list.append(word)
-    in_text= temp_list.copy()
-    temp_list.clear()
-    
-    ### removing stopwords and punctuation marks from in_text
-    for word in in_text:
-        if word not in stopwords.words('english') and word not in string.punctuation:
-            temp_list.append(word)
-    in_text=temp_list.copy()
-    temp_list.clear()
-    
-    ### Stemming the words to get base form of words
-    for word in in_text:
-        temp_list.append(stemmer.stem(word))
-    ### joining all words in list and returning complete sentence/ document
-    return " ".join(temp_list)
 
 
 app=Flask(__name__)
